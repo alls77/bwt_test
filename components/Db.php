2d19292fa1d0242a04a1a5fa549eaa82825d288d
alls77 <alina_selina@bk.ru>
2018-11-13 04:49:58 +0200

    Update Db.php

--- components/Db.php
+++ components/Db.php
@@ -8,7 +8,6 @@ class Db {
 		$params = include($paramsPath);
 
 		$options=array(
-			PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
 			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 			);
