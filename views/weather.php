072af5d6e496e561dc229ea29724c9f369e78fd8
alls77 <alina_selina@bk.ru>
2018-11-13 04:51:40 +0200

    Update weather.php

--- views/weather.php
+++ views/weather.php
@@ -1,6 +1,3 @@
-<h3 style="color:#00008B;text-align:center" >Weather in Zaporizhia:<?php date_default_timezone_set('Europe/Kiev');
-		echo date('d.m.Y');?></h3><br>
-		
 <div class="container">
 	<div class="row">
 		<div class="col-md-offset-2 col-md-8">
@@ -10,6 +7,9 @@
 		use PHPHtmlParser\Dom;
 		
 		if (isset($_SESSION['name'])) {
+			date_default_timezone_set('Europe/Kiev');
+			echo "<h3 style='color:#00008B;text-align:center'>Weather in Zaporizhia: ".date('d.m.Y')."</h3><br>";
+			
 			$client = new GuzzleHttp\Client();
 			
 			$headers = ['User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'];
