24cf38e2d1a191563fb0ad7d3fe928908fb1a9f9
alls77 <alina_selina@bk.ru>
2018-11-13 13:38:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
+
 // FRONT COTROLLER
 include_once('template/header.php');
-// 1. Общие настройки
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
-// 2. Подключение файлов системы
-
-define('ROOT', __DIR__); // C:\htdocs\bwt_test
+define('ROOT', __DIR__);
 require_once(ROOT.'/components/Router.php');
 require_once(ROOT.'/components/Db.php');
 require_once(ROOT.'/vendor/autoload.php');
-
-// 4. Вызор Router
 
 $router = new Router();
 $router->run();
